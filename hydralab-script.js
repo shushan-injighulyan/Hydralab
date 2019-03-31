1d866802dbc03605baf0108300221d28e401ae94
Shushan Injighulyan <shushan.injighulyan@gmail.com>
2019-03-31 23:30:39 +0400

    final

--- hydralab-script.js
+++ hydralab-script.js
@@ -1,119 +1,102 @@
-function checkName() {
-	let letters = /^[A-Za-z]+$/;
-	let nameValue = document.getElementById("inputName").value;
-	if(nameValue.match(letters)) { 
-      return true;
-    } else {
-      return false;
-    }
-}
-
-function checkSurname() {
-	let letters = /^[A-Za-z]+$/;
-	let surnameValue = document.getElementById("inputSurname").value;
-	if(surnameValue.match(letters)) {   
-      return true;
-    } else {
-      return false;
-    }
+function checkFirstName() {
+	let firstNamePattern = /^[A-Za-z]+$/;
+	let firstNameValue = document.getElementById("firstName").value;
+    return ( !!firstNameValue.match(firstNamePattern));
 }
 
 
+function checkLastName() {
+	let lastNamePattern = /^[A-Za-z]+$/;
+	let lastNameValue = document.getElementById("lastName").value;
+	return ( !!lastNameValue.match(lastNamePattern));
+}
 
 
-function checkLogin() {
-	let letters = /^[0-9a-zA-Z]+$/;
-	let loginValue = document.getElementById("inputLogin").value;
-	if(loginValue.match(letters)) {
-      return true;
-    } else {
-      return false;
-      
-    }
+function checkUsername() {
+	let usernamePattern = /^[0-9a-zA-Z]+$/;
+	let usernameValue = document.getElementById("username").value;
+	return(!!usernameValue.match(usernamePattern));
 }
 
 
 function checkPassword() {
-	let letters = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
-	let passwordValue = document.getElementById("inputPassword").value;
-	if(passwordValue.match(letters)) {
- 
-      return true;
-    } else {
-      return false;
-    }
-
-m
-}
-
-
-
-function checkDateFormat() {
-	let letters = /(?=.*\d)(?=.*[/]).{10}/;
-	let dateValue1 = document.getElementById("inputDate").value;
-	if(dateValue1.match(letters) && dateValue1.indexOf("/") ===2 && dateValue1.lastIndexOf("/")===5 ) {
- 
-     checkDate();
-    } else {
-      return false;
-  
-    }
-
-
+	let passwordPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+	let passwordValue = document.getElementById("password").value;
+    return(!!passwordValue.match(passwordPattern));
 }
 
 
 function checkDate() {
-let date = document.getElementById("inputDate").value.split("/");
-let day = Number(date[0]), month =  Number(date[1]), year =  Number(date[2]);
-if(year > 2012) {
-return false;
-} else if ( day > 31 || month > 12  || year < 1900 || ( ( month === 4 ||  month === 6 ||  month === 9 ||  month === 11) && day ===31 ) 
- ||  ( month ===2 && year %4 === 0 &&  day > 29)  || ( month === 2 && year % 4 !== 0 &&  day > 28) ) {
-
-return false;
-} else {
-return true;
+    let date = document.getElementById("yearOfBirth").value;
+    return  (!!(Number(date) > new Date().getFullYear() - 90  &&  date <  new Date().getFullYear() - 10) );
 }
-
-
-
-}
-
-
-
 
 
 function checkPasswordConfirm() {
-   let passwordValue = String(document.getElementById("inputPassword").value);
-   let passConfirm = String(document.getElementById("inputPasswordConfirm").value);
-   if (passConfirm === passwordValue) {
-   	return true;
-   } else {
-   	return false;
-   }
+   let passwordValue = String(document.getElementById("password").value);
+   let passwordConfirmValue = String(document.getElementById("passwordConfirm").value);
+   return (!!(passwordConfirmValue === passwordValue));
 }
 
-function myFunction() {
 
-	if ( checkName() && checkSurname() && checkLogin() && checkPassword() && checkPasswordConfirm() && checkDateFormat()) {
-		document.getElementById("main-form").style.visibility = "hidden";
+function checkFormValidation() {
+
+	if ( checkFirstName() && checkLastName() && checkUsername() && checkPassword() && checkPasswordConfirm() && checkDate()) {
+		document.getElementById("main-form").style.display = "none";
 		let congrats = document.createElement("div");
-		congrats.innerHTML = "Congrats";
+		let name = document.getElementById("firstName").value;
+		congrats.innerHTML = `Congrats ${name}`;
+		congrats.className = "congrats";
 		document.getElementById("body").appendChild(congrats);
+	}
 
-	} else if (!checkName()) {
-		alert ("Plase enter valid Name")
-    } else if (!checkSurname()) {
-		alert ("Plase enter valid Surname")
-    } else if (!checkLogin()) {
-		alert ("Plase enter valid Login")
-    } else if (!checkPassword()) {
-		alert ("Plase enter valid Password")
-    } else if (!checkPasswordConfirm()) {
-		alert ("Yor password doesn't match")
-    }  else if (!checkDateFormat()) {
-		alert ("Plase enter correct date (ex. 01/01/2000)") 
-    } 
+	if (!checkFirstName()) {
+		document.getElementById("firstNameError").style.display = "block";
+    }
+
+    if (checkFirstName()) {
+        document.getElementById("firstNameError").style.display = "none";
+    }
+
+	if (!checkLastName()) {
+        document.getElementById("lastNameError").style.display = "block";
+    }
+
+    if (checkLastName()) {
+        document.getElementById("lastNameError").style.display = "none";
+    }
+
+	if (!checkDate()) {
+        document.getElementById("yearOfBirthError").style.display = "block";
+    }
+
+    if (checkDate()) {
+        document.getElementById("yearOfBirthError").style.display = "none";
+    }
+
+	if (!checkUsername()) {
+        document.getElementById("usernameError").style.display = "block";
+    }
+
+    if (checkUsername()) {
+        document.getElementById("usernameError").style.display = "none";
+    }
+
+	if (!checkPassword()) {
+        document.getElementById("passwordError").style.display = "block";
+    }
+
+	if (checkPassword()) {
+        document.getElementById("passwordError").style.display = "none";
+    }
+
+	if (!checkPasswordConfirm()) {
+        document.getElementById("passwordConfirmError").style.display = "block";
+    }
+
+    if (checkPasswordConfirm()) {
+        document.getElementById("passwordConfirmError").style.display = "none";
+    }
+
 }
 
